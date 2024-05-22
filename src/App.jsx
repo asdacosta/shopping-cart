78854c5604b6be2f0cb262eddefef0f37f581857
asdacosta <asdacosta18@gmail.com>
2024-05-22 17:22:01 +0000

    Add main entry points

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./stylesheets/reset.css";
+import "./stylesheets/App.css";
+import { HomePage } from "./components/HomePage";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
