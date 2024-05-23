919968833a71bf54eb38a8cd0d6c1f048cd86791
asdacosta <asdacosta18@gmail.com>
2024-05-23 16:29:59 +0000

    Fetch from parent component - HomePage.jsx - for performance

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,9 +2,32 @@ import homeStyles from "../stylesheets/HomePage.module.css";
 import { Nav } from "./Nav";
 import { ids } from "./ids";
 import { Item } from "./Item";
+import { useState, useEffect } from "react";
 
 function HomePage() {
-  const items = ids.map((id, index) => <Item key={id} index={index} />);
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://fakestoreapi.com/products/category/" +
+        encodeURIComponent("men's clothing"),
+      { mode: "cors" }
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setResponse(response);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (!response) {
+    return;
+  }
+
+  const items = ids.map((id, index) => (
+    <Item key={id} itemResponse={response[index]} />
+  ));
 
   return (
     <>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,35 +2,8 @@ import { useState } from "react";
 import itemStyles from "../stylesheets/Item.module.css";
 import { useEffect } from "react";
 
-function Item({ index }) {
-  const [details, setDetails] = useState({
-    image: null,
-    title: null,
-    description: null,
-    price: null,
-  });
+function Item({ itemResponse }) {
   const [showDescription, setShowDescription] = useState(false);
-
-  useEffect(() => {
-    fetch(
-      "https://fakestoreapi.com/products/category/" +
-        encodeURIComponent("men's clothing"),
-      { mode: "cors" }
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        const item = response[index];
-        const trimmedDescription = item.description.split(".")[0] + ".";
-        setDetails({
-          image: item.image,
-          title: item.title,
-          description: trimmedDescription,
-          price: item.price,
-        });
-      })
-      .catch((error) => console.error(error));
-  }, []);
 
   function triggerShow() {
     setShowDescription(true);
@@ -48,7 +21,7 @@ function Item({ index }) {
         onMouseOut={triggerHide}
       >
         {showDescription ? (
-          <p>{details.description}</p>
+          <p>{itemResponse.description.split(".")[0] + "."}</p>
         ) : (
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
@@ -57,15 +30,15 @@ function Item({ index }) {
       </span>
       {!showDescription ? (
         <div className={itemStyles.itemImage}>
-          <img src={details.image} alt={details.title} />
+          <img src={itemResponse.image} alt={itemResponse.title} />
         </div>
       ) : (
         <div className={itemStyles.itemImage}></div>
       )}
 
       <div className={itemStyles.itemDetails}>
-        <p>{details.title}</p>
-        <p>$ {details.price}</p>
+        <p>{itemResponse.title}</p>
+        <p>$ {itemResponse.price}</p>
         <button>Add to Cart</button>
       </div>
     </section>
