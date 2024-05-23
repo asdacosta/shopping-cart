d5ffde3ae30c1972e1145f5584e242fa775e1fa2
asdacosta <asdacosta18@gmail.com>
2024-05-23 09:30:33 +0000

    Add other item details

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,6 +4,10 @@ import { useEffect } from "react";
 
 function Item({ index }) {
   const [imageURL, setImageURL] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [showDescription, setShowDescription] = useState(false);
 
   useEffect(() => {
     fetch(
@@ -12,25 +16,46 @@ function Item({ index }) {
       { mode: "cors" }
     )
       .then((response) => response.json())
-      .then((response) => setImageURL(response[index].image))
-      .catch((error) => console.error(error))
-      .finally((response) => console.log(response));
+      .then((response) => {
+        console.log(response);
+        setImageURL(response[index].image);
+        setTitle(response[index].title);
+        setDescription(response[index].description);
+        setPrice(response[index].price);
+      })
+      .catch((error) => console.error(error));
   }, []);
+
+  function triggerShow() {
+    setShowDescription(true);
+  }
+
+  function triggerHide() {
+    setShowDescription(false);
+  }
 
   return (
     <section className={itemStyles.item}>
-      <span className={itemStyles.info}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
-        </svg>
+      <span
+        className={itemStyles.info}
+        onMouseOver={triggerShow}
+        onMouseOut={triggerHide}
+      >
+        {showDescription ? (
+          <p>{description}</p>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+          </svg>
+        )}
       </span>
       <div
         className={itemStyles.itemImage}
         style={{ backgroundImage: `url(${imageURL})` }}
       ></div>
       <div className={itemStyles.itemDetails}>
-        <p>Whatever</p>
-        <p>2000 cedis</p>
+        <p>{title}</p>
+        <p>$ {price}</p>
         <button>Add to Cart</button>
       </div>
     </section>
