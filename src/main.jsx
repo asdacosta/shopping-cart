a8158224f8fb1d4c8b4366cd5be450eb5f593081
asdacosta <asdacosta18@gmail.com>
2024-05-24 00:06:26 +0000

    Make use of client side routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import { ShopPage } from "./components/ShopPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "shop",
+    element: <ShopPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
