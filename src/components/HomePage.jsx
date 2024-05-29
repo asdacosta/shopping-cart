b8679792b1e1c5fd71cccddd49396d29329068c9
asdacosta <asdacosta18@gmail.com>
2024-05-29 06:57:58 +0000

    Introduce a centralized context for all states to access

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
 import { Nav } from "./Nav";
 import { Outlet } from "react-router-dom";
 
+export const ShopContext = createContext({
+  allAdded: [],
+  allQuantity: [],
+  setAllAdded: () => {},
+  setAllQuantity: () => {},
+});
+
 function HomePage() {
-  const [navPage, setNavPage] = useState("home");
+  const [allAdded, setAllAdded] = useState([false, false, false, false]);
+  const [allQuantity, setAllQuantity] = useState([1, 1, 1, 1]);
 
   return (
     <>
       <Nav />
-      <Outlet />
+      <ShopContext.Provider
+        value={{ allAdded, allQuantity, setAllAdded, setAllQuantity }}
+      >
+        <Outlet />
+      </ShopContext.Provider>
     </>
   );
 }
